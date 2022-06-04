da375ec7ff8a83b304634a2534fdf134deec6c4a
TheSampaio <mirandasampaiokellvyn@outlook.com>
2022-06-03 21:46:52 -0300

    Improving all project

--- Shaders/TriangleFragment.glsl
+++ Shaders/TriangleFragment.glsl
@@ -1,14 +1,14 @@
-#version 330 core
+#version 330 core // Specify GLSL's version
 
-in vec3 Color;
-in vec2 UV;
+in vec3 Color; // Variable created to input Color
+in vec2 UV;    // Variable created to input Texture
 
-out vec4 OutColor;
+out vec4 OutColor; // Variable created to output Color
 
-uniform sampler2D TextureSampler;
+uniform sampler2D TextureSampler; // Use Texture in shader program with uniform
 
 void main()
 {
-	vec3 TextureColor = texture(TextureSampler, UV).rgb;
-	OutColor = vec4(TextureColor, 1.0);
+	vec3 TextureColor = texture(TextureSampler, UV).rgb; // Get texture's color
+	OutColor = vec4(TextureColor, 1.0);					 // Get the final color
 }
--- Shaders/TriangleVertex.glsl
+++ Shaders/TriangleVertex.glsl
@@ -1,18 +1,19 @@
-#version 330 core
+#version 330 core // Specify GLSL's version
 
-layout(location = 0) in vec3 InPosition;
-layout(location = 1) in vec3 InColor;
-layout(location = 2) in vec2 InUV;
+layout(location = 0) in vec3 InPosition; // Create layout 0
+layout(location = 1) in vec3 InColor;	 // Create layout 1
+layout(location = 2) in vec2 InUV;		 // Create layout 2
 
-out vec3 Color;
-out vec2 UV;
+out vec3 Color; // Variable created to output Color
+out vec2 UV;	// Variable created to output Texture
 
-uniform mat4 ModelViewProjection;
+uniform mat4 ModelViewProjection; // Use MVP in shader program with uniform
 
 void main()
 {
-	gl_Position = ModelViewProjection * vec4(InPosition, 1.0);
 
-	Color = InColor;
-	UV = InUV;
+	Color = InColor; // Send color to fragment shader
+	UV = InUV;		 // Send texture to fragment shader
+
+	gl_Position = ModelViewProjection * vec4(InPosition, 1.0); // Send vertice's position to fragment shader
 }

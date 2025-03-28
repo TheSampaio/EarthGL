22f426cc619177b0e9fd9e3acfafdc1c6980e563
Kellvyn Sampaio <mirandasampaiokellvyn@outlook.com>
2025-03-28 15:44:03 -0300

    Hard Reset

--- source/Types.cpp
+++
@@ -1,9 +0,0 @@
-#include "PCH.hpp"
-#include "Types.hpp"
-
-Vertex::Vertex(glm::vec3 position, glm::vec3 normal, glm::vec2 texture)
-{
-    this->position = position;
-    this->normal = normal;
-    this->texture = texture;
-}
